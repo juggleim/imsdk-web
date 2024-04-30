6656d72041717602703f4b15eae68f0758c01d10
luotm <shadowai5200@gmail.com>
2024-04-30 19:21:39 +0800

    fix: uid 长度

--- src/utils.js
+++ src/utils.js
@@ -317,7 +317,7 @@ const getBrowser = () => {
 };
 
 const getUUID = () => {
-  return 'j' + 'xxxx-xxxx'.replace(/[xy]/g, function (c) {
+  return 'j' + 'xxxx-xxxx-xxxx'.replace(/[xy]/g, function (c) {
     let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });

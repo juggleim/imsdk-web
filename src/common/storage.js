bc161ee000ed00142f50aee628a651908cf4294f
luotm <shadowai5200@gmail.com>
2024-08-05 14:56:01 +0800

    fix: Storage 内存缓存 kv, 兼容 Web 开启多 Tab 情况

--- src/common/storage.js
+++ src/common/storage.js
@@ -1,6 +1,8 @@
 import { STORAGE } from "../enum";
 import utils from "../utils";
+import Cacher from "./cache";
 
+let storageCacher = Cacher();
 // 动态设置 storage key 前缀，例如 _appkey_userid_
 let _storage_private_prefix_ = '';
 let getKey = (key) => {
@@ -10,19 +12,30 @@ let getKey = (key) => {
 let set = (key, value) => {
   let _key = getKey(key);
   let storage = { data: value};
+  storageCacher.set(_key, storage);
   localStorage.setItem(_key, utils.toJSON(storage));
 };
 
 let get = (key) => {
   let _key = getKey(key);
+
+  let _storage = storageCacher.get(_key);
+  let _value = _storage.data;
+  if(!utils.isUndefined(_value)){
+    return _value;
+  }
   let storage = localStorage.getItem(_key);
   storage = utils.parse(storage) || { data: {}};
+
+  storageCacher.set(_key, storage);
+
   let value = storage.data;
   return value;
 }
 
 let remove = (key) => {
   let _key = getKey(key);
+  storageCacher.remove(key);
   localStorage.removeItem(_key);
 }
 

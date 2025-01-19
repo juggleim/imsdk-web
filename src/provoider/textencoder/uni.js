ed373d5e017d38b894b5b5144110a34d8c9c5ce4
luotm <shadowai5200@gmail.com>
2025-01-19 17:08:29 +0800

    feat: 解析 Decoder

--- src/provoider/textencoder/uni.js
+++ src/provoider/textencoder/uni.js
@@ -1,10 +1,12 @@
+import utils from "../../utils";
 export default function(){
   let encoder = (str) => {
     return uni.base64ToArrayBuffer(str);
   };
 
   let decoder = (buffer) => {
-    return uni.arrayBufferToBase64(buffer);
+    let str = uni.arrayBufferToBase64(buffer);
+    return utils.decodeBase64(str);
   };
 
   return {
